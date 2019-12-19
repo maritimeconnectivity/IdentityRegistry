bfc1e210d666049fefbf15e0065128f56d999724
Oliver Haagh <oliver@dmc.international>
2019-12-19 12:31:28 +0100

    updated version to 0.10.0

--- src/main/java/net/maritimecloud/identityregistry/config/SwaggerConfig.java
+++ src/main/java/net/maritimecloud/identityregistry/config/SwaggerConfig.java
@@ -48,7 +48,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("Maritime Connectivity Platform Identity Registry API")
                 .description("MCP Identity Registry API can be used for managing entities in the Maritime Connectivity Platform.")
-                .version("0.9.1")
+                .version("0.10.0")
                 .contact(new Contact("Maritime Connectivity Platform", "http://maritimeconnectivity.net", "info@maritimeconnectivity.net"))
                 .license("Apache 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
