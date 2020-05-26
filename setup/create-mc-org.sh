a3ce946b037204e42929155c495c5b1fafdbbcb5
Oliver Haagh <oliver@dmc.international>
2020-05-26 12:37:56 +0200

    Refactoring of config files

--- setup/create-mc-org.sh
+++ setup/create-mc-org.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to insert an organization into the identity registry DB, used for bootstraping.
+# Script to insert an organization into the identity registry DB, used for bootstrapping.
 #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
--- setup/guide/sample-conf/update-crl.sh
+++ setup/guide/sample-conf/update-crl.sh
@@ -1,8 +1,8 @@
 echo
 echo "Running CRL update"
 date
-curl -L "https://api.example.com/x509/api/certificates/crl/urn:mrn:mcl:ca:maritimecloud" > crl/mcp-root-crl.pem
-curl -L "https://api.example.com/x509/api/certificates/crl/urn:mrn:mcl:ca:mcp-idreg" > crl/mcp-idreg-crl.pem
+curl -L "https://api.example.com/x509/api/certificates/crl/urn:mrn:mcp:ca:idp1:mcp" > crl/mcp-root-crl.pem
+curl -L "https://api.example.com/x509/api/certificates/crl/urn:mrn:mcp:ca:idp1:mcp-idreg" > crl/mcp-idreg-crl.pem
 
 cat crl/mcp-idreg-crl.pem crl/mc-root-crl.pem > crl/combined-crl.pem
 
