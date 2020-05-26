44cd0bae0051b570b45313b5bab73352e3dc77ac
Oliver Haagh <oliver@dmc.international>
2020-05-26 10:07:01 +0200

    Refactoring of config files

--- setup/create-mc-org.sql
+++ setup/create-mc-org.sql
@@ -1,7 +1,7 @@
 USE identity_registry;
-#--- Create the Maritime Cloud organization
+#--- Create the Bootstrap organization
 INSERT INTO `organizations` (name, mrn, email, address, country, url, approved, federation_type, certificate_authority, created_at, updated_at) VALUES
-    ('Maritime Cloud', 'urn:mrn:mcl:org:maritimecloud', 'info@maritimecloud.net', '1 Martime Street, The Eight Ocean', 'Denmark', 'http://maritimecloud.net', 1, 'external-idp', 'urn:mrn:mcl:ca:maritimecloud-idreg', NOW(), NOW());
-#--- Create the roles for the Maritime Cloud organization
+    ('Bootstrap Org', 'urn:mrn:mcp:org:idp1:bootstrap', 'info@maritimeconnectivity.net', '1 Maritime Street, The Eight Ocean', 'Denmark', 'https://maritimeconnectivity.net', 1, 'external-idp', 'urn:mrn:mcp:ca:idp1:mcp-idreg', NOW(), NOW());
+#--- Create the roles for the Bootstrap organization
 INSERT INTO `roles` (id_organization, role_name, permission, created_at, updated_at) VALUES
-    ((SELECT id FROM `organizations` WHERE mrn='urn:mrn:mcl:org:maritimecloud'), 'ROLE_SITE_ADMIN', 'MCADMIN', NOW(), NOW());
+    ((SELECT id FROM `organizations` WHERE mrn='urn:mrn:mcp:org:idp1:bootstrap'), 'ROLE_SITE_ADMIN', 'MCPADMIN', NOW(), NOW());
