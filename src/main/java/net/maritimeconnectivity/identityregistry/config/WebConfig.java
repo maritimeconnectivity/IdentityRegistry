a33457c6486eab3d9888e4e9ff4df9beea8db370
Oliver Haagh <oliver@dmc.international>
2020-06-03 16:06:24 +0200

    Fixed issue where percent encoded slashes could not be used in MRN's

--- src/main/java/net/maritimeconnectivity/identityregistry/config/WebConfig.java
+++ src/main/java/net/maritimeconnectivity/identityregistry/config/WebConfig.java
@@ -20,8 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
@@ -36,4 +38,10 @@ public class WebConfig extends WebMvcConfigurationSupport {
         return hm;
     }
 
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
 }
