408956c35d5a2fea641374aa01616063043e3527
Oliver Haagh <oliver@dmc.international>
2020-06-17 14:00:47 +0200

    Fixed issue with percent encoded slashes in URL's and make smtp port configurable in unit tests

--- src/main/java/net/maritimeconnectivity/identityregistry/config/WebConfig.java
+++ src/main/java/net/maritimeconnectivity/identityregistry/config/WebConfig.java
@@ -20,8 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
@@ -36,4 +38,11 @@ public class WebConfig extends WebMvcConfigurationSupport {
         return hm;
     }
 
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // Allow encoded "/" (%2F) in urls without actually interpreting it immediately as a path separator
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
 }
