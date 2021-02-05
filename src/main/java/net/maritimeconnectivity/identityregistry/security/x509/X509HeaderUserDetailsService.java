ae0c1b7635bfd406d302d688bfa3d47af737bc63
Oliver Haagh <ohaagh@gmail.com>
2021-02-05 16:38:00 +0100

    Update version number and use Lombok generated log instead of creating one manually

--- src/main/java/net/maritimeconnectivity/identityregistry/security/x509/X509HeaderUserDetailsService.java
+++ src/main/java/net/maritimeconnectivity/identityregistry/security/x509/X509HeaderUserDetailsService.java
@@ -15,6 +15,7 @@
  */
 package net.maritimeconnectivity.identityregistry.security.x509;
 
+import lombok.extern.slf4j.Slf4j;
 import net.maritimeconnectivity.identityregistry.model.database.Organization;
 import net.maritimeconnectivity.identityregistry.model.database.Role;
 import net.maritimeconnectivity.identityregistry.model.database.entities.User;
@@ -23,8 +24,6 @@ import net.maritimeconnectivity.identityregistry.services.OrganizationService;
 import net.maritimeconnectivity.identityregistry.services.RoleService;
 import net.maritimeconnectivity.pki.CertificateHandler;
 import net.maritimeconnectivity.pki.PKIIdentity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -39,6 +38,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Slf4j
 @Service("userDetailsService")
 public class X509HeaderUserDetailsService implements UserDetailsService {
 
@@ -49,24 +49,22 @@ public class X509HeaderUserDetailsService implements UserDetailsService {
     @Autowired
     private EntityService<User> userService;
 
-    private static final Logger logger = LoggerFactory.getLogger(X509HeaderUserDetailsService.class);
-
     @Override
     public UserDetails loadUserByUsername(String certificateHeader) {
         if (certificateHeader == null || certificateHeader.length() < 10) {
-            logger.warn("No certificate header found");
+            log.warn("No certificate header found");
             throw new UsernameNotFoundException("No certificate header found");
         }
         X509Certificate userCertificate = CertificateHandler.getCertFromNginxHeader(certificateHeader);
         if (userCertificate == null) {
-            logger.error("Extracting certificate from header failed");
+            log.error("Extracting certificate from header failed");
             throw new UsernameNotFoundException("Extracting certificate from header failed");
         }
 
         // Get user details from the certificate
         PKIIdentity user = CertificateHandler.getIdentityFromCert(userCertificate);
         if (user == null) {
-            logger.warn("Extraction of data from the certificate failed");
+            log.warn("Extraction of data from the certificate failed");
             throw new UsernameNotFoundException("Extraction of data from the client certificate failed");
         }
         // Convert the PKIIdentity to a user object Spring can read
@@ -87,18 +85,18 @@ public class X509HeaderUserDetailsService implements UserDetailsService {
         if (user.getMrn() != null && user.getO() != null && user.getOu().equals("user")) {
             Organization org = organizationService.getOrganizationByMrn(user.getO());
             if (org == null) {
-                logger.error("The Organization is unknown!");
+                log.error("The Organization is unknown!");
                 throw new UsernameNotFoundException("The Organization is unknown!");
             }
             User mirUser = userService.getByMrn(user.getMrn());
             if (mirUser == null || !mirUser.getIdOrganization().equals(org.getId())) {
-                logger.error("The User is unknown!");
+                log.error("The User is unknown!");
                 throw new UsernameNotFoundException("The User is unknown!");
             }
             if (mirUser.getPermissions() != null) {
                 String[] permissions = mirUser.getPermissions().split(",");
                 for(String permission: permissions) {
-                    logger.debug("Looking up role: {}", permission);
+                    log.debug("Looking up role: {}", permission);
                     List<Role> foundRoles = roleService.getRolesByIdOrganizationAndPermission(org.getId(), permission);
                     if (foundRoles != null) {
                         for (Role foundRole : foundRoles) {
