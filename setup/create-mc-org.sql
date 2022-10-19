586afba457841669fedac188d4d1628b468c18df
Oliver Haagh <oliver@dmc.international>
2022-10-19 10:22:25 +0200

    Refactor bootstrap sql script Remove newlines and use the boolean value *true* instead of 1 for the value of *approved*

--- setup/create-mc-org.sql
+++ setup/create-mc-org.sql
@@ -1,6 +1,4 @@
 #--- Create the Bootstrap organization
-INSERT INTO `organizations` (name, mrn, email, address, country, url, approved, federation_type, certificate_authority, created_at, updated_at) VALUES
-    ('Bootstrap Org', 'urn:mrn:mcp:org:idp1:bootstrap', 'info@maritimeconnectivity.net', '1 Maritime Street, The Eight Ocean', 'Denmark', 'https://maritimeconnectivity.net', 1, 'external-idp', 'urn:mrn:mcp:ca:idp1:mcp-idreg', NOW(), NOW());
+INSERT INTO `organizations` (name, mrn, email, address, country, url, approved, federation_type, certificate_authority, created_at, updated_at) VALUES ('Bootstrap Org', 'urn:mrn:mcp:org:idp1:bootstrap', 'info@maritimeconnectivity.net', '1 Maritime Street, The Eight Ocean', 'Denmark', 'https://maritimeconnectivity.net', true, 'external-idp', 'urn:mrn:mcp:ca:idp1:mcp-idreg', NOW(), NOW());
 #--- Create the roles for the Bootstrap organization
-INSERT INTO `roles` (id_organization, role_name, permission, created_at, updated_at) VALUES
-    ((SELECT id FROM `organizations` WHERE mrn='urn:mrn:mcp:org:idp1:bootstrap'), 'ROLE_SITE_ADMIN', 'MCPADMIN', NOW(), NOW());
+INSERT INTO `roles` (id_organization, role_name, permission, created_at, updated_at) VALUES ((SELECT id FROM `organizations` WHERE mrn='urn:mrn:mcp:org:idp1:bootstrap'), 'ROLE_SITE_ADMIN', 'MCPADMIN', NOW(), NOW());
