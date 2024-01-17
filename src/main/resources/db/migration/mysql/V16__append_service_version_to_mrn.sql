b8ea1664ab2f8aa152a71a53b9a63fa3eeb4f9fb
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2024-01-17 16:18:54 +0100

    We should also indicate that we have updated the DB entries

--- src/main/resources/db/migration/mysql/V16__append_service_version_to_mrn.sql
+++ src/main/resources/db/migration/mysql/V16__append_service_version_to_mrn.sql
@@ -1,4 +1,4 @@
 BEGIN;
-UPDATE `services` SET `mrn` = CONCAT(`mrn`, ':', `instance_version`) WHERE `instance_version` != '';
+UPDATE `services` SET `mrn` = CONCAT(`mrn`, ':', `instance_version`), `updated_at` = NOW() WHERE `instance_version` != '';
 ALTER TABLE `services` DROP COLUMN `instance_version`;
 COMMIT;
--- src/main/resources/db/migration/postgresql/V4__append_service_version_to_mrn.sql
+++ src/main/resources/db/migration/postgresql/V4__append_service_version_to_mrn.sql
@@ -1,5 +1,5 @@
 BEGIN;
-UPDATE services SET mrn = mrn || ':' || instance_version WHERE instance_version != '';
+UPDATE services SET mrn = mrn || ':' || instance_version, updated_at = now() WHERE instance_version != '';
 ALTER TABLE services DROP COLUMN instance_version CASCADE;
 ALTER TABLE services ADD CONSTRAINT unique_mrn UNIQUE (mrn);
 COMMIT;
