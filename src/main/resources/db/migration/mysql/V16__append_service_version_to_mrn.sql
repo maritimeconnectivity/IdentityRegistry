629f6e7129d06c8b976d5c7837f5780907a61ecc
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2024-01-17 10:25:31 +0100

    MySQL uses the `CONCAT()` function for string concatenation

--- src/main/resources/db/migration/mysql/V16__append_service_version_to_mrn.sql
+++ src/main/resources/db/migration/mysql/V16__append_service_version_to_mrn.sql
@@ -1,4 +1,4 @@
 BEGIN;
-UPDATE `services` SET `mrn` = `mrn` + ':' + `instance_version` WHERE `instance_version` != '';
+UPDATE `services` SET `mrn` = CONCAT(`mrn`, ':', `instance_version`) WHERE `instance_version` != '';
 ALTER TABLE `services` DROP COLUMN `instance_version`;
 COMMIT;
